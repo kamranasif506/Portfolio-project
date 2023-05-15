d9dbab4e4b51862462ab5936966030af98033ebc
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-15 22:12:04 +0500

    Removed eslint errors

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const openNav = document.getElementById('bar_icon');
-openNav.addEventListener('click', function() {
-  console.log('Button clicked!');
-  document.getElementById("myNav").style.width = "100%";
+openNav.addEventListener('click', () => {
+  document.getElementById('myNav').style.width = '100%';
 });
 
 const closeNav = document.getElementById('close_icon');
-closeNav.addEventListener('click', function() {
-  console.log('Button clicked!');
-  document.getElementById("myNav").style.width = "0%";
+closeNav.addEventListener('click', () => {
+  document.getElementById('myNav').style.width = '0%';
 });
