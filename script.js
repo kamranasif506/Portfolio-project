c2634bdf5e2523064d91b9afa53d490f0c0890fc
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-15 22:30:55 +0500

    Added functionality for close mobile menu when click on nav links

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const closeNav = document.getElementById('close_icon');
 closeNav.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '0%';
 });
+let menuItems = document.getElementsByClassName('navlinks-items');
+Array.from(menuItems).forEach(li => {
+  li.addEventListener('click', handleClick);
+});
+function handleClick(event) {
+  document.getElementById('myNav').style.width = '0%';
+}
