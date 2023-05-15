366f0b9783dd57975ed5be8081d34b6b922332af
Cristhian Castillo <97223412+Kal2403@users.noreply.github.com>
2023-05-15 17:11:50 +0200

    Added close menu function

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ openNav.addEventListener('click', function() {
   console.log('Button clicked!');
   document.getElementById("myNav").style.width = "100%";
 });
+
+const closeNav = document.getElementById('close_icon');
+console.log('sad');
+// Add a click event handler
+closeNav.addEventListener('click', function() {
+  // Code to be executed when the button is clicked
+  console.log('Button clicked!');
+  document.getElementById("myNav").style.width = "0%";
+});
