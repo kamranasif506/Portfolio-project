d5a35d6fdf048565f9a3d099653a31d3cdab9e67
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-15 22:09:19 +0500

    Removed unwanted lines from script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const openNav = document.getElementById('bar_icon');
-console.log('sad');
-// Add a click event handler
 openNav.addEventListener('click', function() {
-  // Code to be executed when the button is clicked
   console.log('Button clicked!');
   document.getElementById("myNav").style.width = "100%";
 });
 
 const closeNav = document.getElementById('close_icon');
-console.log('sad');
-// Add a click event handler
 closeNav.addEventListener('click', function() {
-  // Code to be executed when the button is clicked
   console.log('Button clicked!');
   document.getElementById("myNav").style.width = "0%";
 });
