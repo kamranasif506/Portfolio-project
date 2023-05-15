15712091dad59fb654c1683babae48cc20c4cf7b
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-15 22:32:15 +0500

    Fix linters eslint issues

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const closeNav = document.getElementById('close_icon');
 closeNav.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '0%';
 });
-let menuItems = document.getElementsByClassName('navlinks-items');
-Array.from(menuItems).forEach(li => {
-  li.addEventListener('click', handleClick);
+const menuItems = document.getElementsByClassName('navlinks-items');
+Array.from(menuItems).forEach((li) => {
+  li.addEventListener('click', () => {
+    document.getElementById('myNav').style.width = '0%';
+  });
 });
-function handleClick(event) {
-  document.getElementById('myNav').style.width = '0%';
-}
