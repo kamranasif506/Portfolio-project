86766b11482f6221346fef39461cb57b8bb66601
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-16 17:57:50 +0500

    projects data stored in array

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const openNav = document.getElementById('bar_icon');
-const take_tour = document.getElementById('take_tour');
 const modal = document.getElementById('myModal');
 openNav.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '100%';
@@ -15,6 +14,68 @@ Array.from(menuItems).forEach((li) => {
     document.getElementById('myNav').style.width = '0%';
   });
 });
-take_tour.addEventListener('click',function(){
-  modal.style.display = "block";
-})
+
+project_lists = {
+  project1:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  },
+  project2:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  },
+  project3:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  },
+  project4:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  },
+  project5:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  },
+  project6:{
+    name:'Project Name goes here',
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    technologies: ['HTML/CSS','Ruby on Rails','Javascript'],
+    image: 'images/Rectangle 34.png',
+    live_link: '',
+    source_link: '',
+  }
+};
+
+const take_tour = document.getElementsByClassName('take_tour');
+Array.from(take_tour).forEach(take_tour => {
+  take_tour.addEventListener('click', function(event){
+    var index = event.target.getAttribute('data-id');
+    const keys = Object.keys(project_lists);
+    const project = project_lists[keys.find(key => key === index)];
+    modal.style.display = "block";
+  });
+});
+// function handleClick(event){
+  
+// }
+
