cc6d5c16feb979022dd86adf3300341e7b91a97d
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-17 18:07:42 +0500

    Removed wrong validation for input message

--- script.js
+++ script.js
@@ -348,15 +348,7 @@ form.addEventListener('submit', function(event) {
   }
   
   
-  if(messageInput.value.trim() == ''){
-    error = 'Please enter your message';
-    errorMessage.innerHTML = error;
-    errorMessage.display='block';
-    contact.style.height = '44.5rem';
-    send_feed.style.top = '41rem';
-    messageInput.focus();
-    return;
-  }
+ 
   errorMessage.innerHTML='';
   contact.style.height = '43.5rem';
   send_feed.style.top = '40rem';
